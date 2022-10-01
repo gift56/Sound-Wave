2a06ba52c87787ed994d7c16faaefe8f6943e4f3
gift56 <efeasiughu@gmail.com>
2022-10-01 23:27:18 +0100

    making use of song component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -22,7 +22,9 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song) => (
+          <SongCard key={song.key} song={song} />
+        ))}
       </div>
     </div>
   );
