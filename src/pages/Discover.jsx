7ad455d43b23a795ae658779d086b35beb017ada
gift56 <efeasiughu@gmail.com>
2022-10-01 23:21:09 +0100

    done adding class name to select

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -9,9 +9,11 @@ const Discover = () => {
         <h2 className="font-bold text-3xl text-white text-left">
           Discover {genreTitle}
         </h2>
-        <select onChange={() => {}} value="" className="bg-black">
-
-        </select>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        ></select>
       </div>
     </div>
   );
