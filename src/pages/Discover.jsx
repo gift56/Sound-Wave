0178e20812349fe785fb978f9159773ac407bd7b
gift56 <efeasiughu@gmail.com>
2022-10-01 23:17:18 +0100

    added title

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,7 +4,10 @@ import { genres } from "../assets/constants";
 const Discover = () => {
   return (
     <div className="flex flex-col">
-      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10"></div>
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+        <select></select>
+      </div>
     </div>
   );
 };
