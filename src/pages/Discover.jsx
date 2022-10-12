7f24c7306cedebc312158a4281ac9d282a27d471
gift56 <efeasiughu@gmail.com>
2022-10-12 14:55:25 +0100

    added some stuff

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,6 +3,7 @@ import { genres } from "../assets/constants";
 
 const Discover = () => {
   const genreTitle = "Pop";
+  
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
