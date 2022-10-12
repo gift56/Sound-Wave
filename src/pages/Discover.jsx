3fe4fd60a187d1dfcfbdc794588d679b89ca05af
gift56 <efeasiughu@gmail.com>
2022-10-12 15:56:52 +0100

    imported endpoints in discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,10 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
   const genreTitle = "Pop";
-  
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
