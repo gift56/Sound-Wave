15f1868688b5a3c2c455cd25cd75d1515a9e897d
gift56 <efeasiughu@gmail.com>
2022-10-12 15:40:48 +0100

    added core

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import playerReducer from './features/playerSlice';
-import { shazamCoreApi } from './services/shazamCore';
+import playerReducer from "./features/playerSlice";
+import { shazamCoreApi } from "./services/shazamCore";
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
 });
