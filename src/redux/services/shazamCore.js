5656d665c8a515f5abdc8a1a7edeb4d5c963a824
gift56 <efeasiughu@gmail.com>
2022-10-12 15:30:00 +0100

    just got api endpoints

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,12 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "59f581e1famshd586a5c442660c7p138848jsne1ca787bf72c",
+    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+  },
+};
+
+fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
