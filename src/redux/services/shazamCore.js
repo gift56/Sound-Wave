14eb265e80236fdeb8bbf0df3852c7ba1837f3f7
gift56 <efeasiughu@gmail.com>
2022-10-12 15:47:33 +0100

    done with redux toolkit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,21 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "59f581e1famshd586a5c442660c7p138848jsne1ca787bf72c",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core.p.rapidapi.com/v1/charts/world",
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1/",
+    prepareHeaders: () => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "59f581e1famshd586a5c442660c7p138848jsne1ca787bf72c"
+      );
+    },
   }),
 });
