597380c2ffffc89caa54384371269a3198bb34b1
gift56 <efeasiughu@gmail.com>
2022-10-13 19:56:15 +0100

    added some animation

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,6 +3,13 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({i, song }) => <div>SongCard</div>;
+const SongCard = ({ i, song }) => (
+  <div
+    key={i}
+    className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+  >
+    <div></div>
+  </div>
+);
 
 export default SongCard;
