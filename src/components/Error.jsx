cfa9d87b3461728aae93db2c7f5467e5a4bd45c3
gift56 <efeasiughu@gmail.com>
2022-10-13 19:46:53 +0100

    done with erros

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { TbMusicOff } from "react-icons/tb";
 
 const Error = () => (
-  <div className="w-full flex justify-center items-center">
+  <div className="w-full flex justify-center items-center gap-4 flex-col">
+    <TbMusicOff size={80} className="text-red-600" />
     <h1 className="font-bold text-2xl text-white">
       Something went wrong. Please try again.
     </h1>
