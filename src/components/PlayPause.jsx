ebaa48e92c840be19ff3c41927e5f485196a1928
gift56 <efeasiughu@gmail.com>
2022-10-13 20:46:06 +0100

    received all props

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -2,9 +2,9 @@ import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
 const PlayPause = ({ song, handlePause, handlePlay, isPlaying, activeSong }) =>
   isPlaying && activeSong?.title === song.title ? (
-    <FaPauseCircle />
+    <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
   ) : (
-    <FaPlayCircle />
+    <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
   );
 
 export default PlayPause;
