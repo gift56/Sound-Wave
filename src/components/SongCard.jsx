1016e3127fd135c4d67f131ec09b09f3bad329f0
gift56 <efeasiughu@gmail.com>
2022-10-13 19:50:51 +0100

    done imported playpause components

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong} from "../redux/features/playerSlice";
 
 const SongCard = () => <div>SongCard</div>;
 
