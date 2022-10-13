825f8fcabd0c790c6800f70977de9e0bfe0fc958
gift56 <efeasiughu@gmail.com>
2022-10-13 20:58:14 +0100

    imported some stuff items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
+import { logo } from "../assets";
+import { links } from "../assets/constants";
 
 const Sidebar = () => <div>Sidebar</div>;
 
