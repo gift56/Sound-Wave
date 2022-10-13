efe5e3c8004074e076e0c12d351aed41119e5a12
gift56 <efeasiughu@gmail.com>
2022-10-13 19:28:40 +0100

    removing comments

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,7 @@ const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
 
+  console.log(data);
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -25,7 +26,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {data.map((song, i) => (
+        {[1,2,3,4,5].map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
