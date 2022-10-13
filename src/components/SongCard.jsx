3005020589f187e45ccba731d3bddca8b2fe4306
gift56 <efeasiughu@gmail.com>
2022-10-13 20:07:57 +0100

    calling play pause functionality

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -17,7 +17,9 @@ const SongCard = ({ i, song }) => {
               ? "flex bg-black bg-opacity-70"
               : "hidden"
           }`}
-        ></div>
+        >
+          <PlayPause />
+        </div>
       </div>
     </div>
   );
