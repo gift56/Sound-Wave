6bf403dbc13774720d037b46211bb1bad26f6987
gift56 <efeasiughu@gmail.com>
2022-10-13 20:34:28 +0100

    passed in two props

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => {
   const dispatch = useDispatch();
-  const {} = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
 
@@ -33,7 +33,13 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
         ))}
       </div>
     </div>
