8704867f6835eb6771f74bf5fd8ef1a3efd22f5e
gift56 <efeasiughu@gmail.com>
2022-10-13 19:57:15 +0100

    added div styings

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -8,7 +8,9 @@ const SongCard = ({ i, song }) => (
     key={i}
     className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
   >
-    <div></div>
+    <div className="relative w-full h-56 group">
+      
+    </div>
   </div>
 );
 
