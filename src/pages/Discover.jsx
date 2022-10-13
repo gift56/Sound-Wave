d25a226d32df4149de3b9bc568de3cc9a67dce18
gift56 <efeasiughu@gmail.com>
2022-10-13 19:30:45 +0100

    use error components

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,6 +7,9 @@ const Discover = () => {
   const genreTitle = "Pop";
 
   if (isFetching) return <Loader title="Loading Songs...." />;
+
+  if (error) return <Error  />;
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
