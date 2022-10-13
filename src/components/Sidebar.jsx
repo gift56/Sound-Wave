ae2ca8fb3c41b31cf859e94ffef271069ee4431d
gift56 <efeasiughu@gmail.com>
2022-10-13 20:57:31 +0100

    imported some stuff

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
 
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+const Sidebar = () => <div>Sidebar</div>;
 
 export default Sidebar;
