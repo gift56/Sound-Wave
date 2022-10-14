5bcb06c25a84c91c233b50b64d786ed3036e7cf6
gift56 <efeasiughu@gmail.com>
2022-10-14 15:59:58 +0100

    importing song query

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
   const { songid } = useParams();
@@ -15,7 +16,7 @@ const SongDetails = () => {
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
         <div className="mt-5">
-            
+
         </div>
       </div>
     </div>
