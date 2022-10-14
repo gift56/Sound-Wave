1c085f4f4eb2d292cca9bc10ea04a3723b6b995f
gift56 <efeasiughu@gmail.com>
2022-10-14 15:45:59 +0100

    importing some components

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 
 const SongDetails = () => {
   const { songid } = useParams();
 
-  console.log(songid)
+  console.log(songid);
   return <div>SongDetails</div>;
 };
 
