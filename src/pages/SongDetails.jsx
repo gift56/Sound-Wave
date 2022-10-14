b8e25f312492920b57df96ae3e211bf1eb6f744d
gift56 <efeasiughu@gmail.com>
2022-10-14 15:48:07 +0100

    added some stuff

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
 
 const SongDetails = () => {
   const { songid } = useParams();
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
   console.log(songid);
   return <div>SongDetails</div>;
