5ca7f55e7d26aa4fbc56627b8225135ef1606536
gift56 <efeasiughu@gmail.com>
2022-10-14 12:08:52 +0100

    importing some components

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode } from "swiper";
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
 import "swiper/css";
 import "swiper/css/free-mode";
 
