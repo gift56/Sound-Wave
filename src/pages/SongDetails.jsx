6b370c6d900f14ecedc9594e53764dd5ee11b33c
gift56 <efeasiughu@gmail.com>
2022-10-14 15:53:27 +0100

    working ongotton song details

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -14,6 +14,9 @@ const SongDetails = () => {
       {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+        <div className="mt-5">
+            
+        </div>
       </div>
     </div>
   );
