28776c037c0f7266e786fec2eb4bcc7924deadcc
gift56 <efeasiughu@gmail.com>
2022-10-14 12:08:05 +0100

    imported swiper css

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -2,6 +2,9 @@ import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
 
 const TopPlay = () => <div>TopPlay</div>;
 
