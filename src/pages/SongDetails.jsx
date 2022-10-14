bcf710d50998fea0cde7479b345a4d26a1707400
gift56 <efeasiughu@gmail.com>
2022-10-14 15:44:22 +0100

    console loging params

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,5 +1,10 @@
 import { useParams } from "react-router-dom";
 
-const SongDetails = () => <div>SongDetails</div>;
+const SongDetails = () => {
+  const { songid } = useParams();
+
+  console.log(songid)
+  return <div>SongDetails</div>;
+};
 
 export default SongDetails;
