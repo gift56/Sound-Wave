22664eea810f638c6109974a701bd332ca688949
gift56 <efeasiughu@gmail.com>
2022-10-14 11:17:47 +0100

    adding bg color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px]"></div>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]"></div>
     </>
   );
 };
