96bad7178e6079d6a90eed2f67b215c34d81a9e8
gift56 <efeasiughu@gmail.com>
2022-10-15 10:28:14 +0100

    working on new stuff

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -4,6 +4,7 @@ const DetailsHeader = ({ songData, artistId, artistData }) => {
   return (
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+      <div className="absolute inset-0 flex items-center"></div>
     </div>
   );
 };
