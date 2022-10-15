7c214c96bb4266a0ae44111b8188c5b85c7e2120
gift56 <efeasiughu@gmail.com>
2022-10-15 10:17:17 +0100

    adding some classes to p tag

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -20,9 +20,15 @@ const SongDetails = () => {
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
         <div className="mt-5">
           {songData?.sections[1].type === "LYRICS" ? (
-            songData?.sections[1].text.map((line, i) => <p key={i} className="">{line}</p>)
+            songData?.sections[1].text.map((line, i) => (
+              <p key={i} className="text-gray-400 text-base my-1">
+                {line}
+              </p>
+            ))
           ) : (
-            <p className="">Sorry, no lyrics found!</p>
+            <p className="text-gray-400 text-base my-1">
+              Sorry, no lyrics found!
+            </p>
           )}
         </div>
       </div>
