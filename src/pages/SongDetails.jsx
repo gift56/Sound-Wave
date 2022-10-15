6240cc570ae8c832c0bc58a643f9ff75649e70c5
gift56 <efeasiughu@gmail.com>
2022-10-15 10:11:47 +0100

    gooton some lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -11,14 +11,20 @@ const SongDetails = () => {
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid });
 
-//   console.log(songData);
+  //   console.log(songData);
 
   return (
     <div className="flex flex-col">
       {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-        <div className="mt-5">{songData?.sections[1].type==="LYRICS"}</div>
+        <div className="mt-5">
+          {songData?.sections[1].type === "LYRICS" ? (
+            songData?.sections[1].text.map((line, i) => <p key={i}>{line}</p>)
+          ) : (
+            <p>Sorry, no lyrics found!</p>
+          )}
+        </div>
       </div>
     </div>
   );
