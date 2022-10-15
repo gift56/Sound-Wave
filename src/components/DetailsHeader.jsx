6bd2b01f8bc8313068c6272a001b6e12114bc0c4
gift56 <efeasiughu@gmail.com>
2022-10-15 10:33:06 +0100

    added some logic in images

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -5,7 +5,7 @@ const DetailsHeader = ({ songData, artistId, artistData }) => {
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
       <div className="absolute inset-0 flex items-center">
-        <img src="" alt="art" />
+        <img src={artistId?artistData?.artists[artistId].attributes?.artwork?.url.replace("{w}","500").replace("{h}","500"):songData?.images?.coverart} alt="art" />
       </div>
     </div>
   );
