273b58c5b9c5d96fdd3c6359174c5ab93cbca30c
gift56 <efeasiughu@gmail.com>
2022-10-15 10:25:22 +0100

    adding some stuff

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 
 const DetailsHeader = ({ songData, artistId, artistData }) => {
-  return <div className="relative w-full flex flex-col"></div>;
+  return (
+    <div className="relative w-full flex flex-col">
+      <div></div>
+    </div>
+  );
 };
 
 export default DetailsHeader;
