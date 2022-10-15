387a2690997e4d10eb0de2775e71107227756acc
gift56 <efeasiughu@gmail.com>
2022-10-15 18:37:56 +0100

    working on object

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -19,22 +19,6 @@ const ArtistDetails = () => {
   return (
     <div className="flex flex-col">
       <DetailsHeader artistId={artistId} artistData={artistData} />
-      <div className="mb-10">
-        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-        <div className="mt-5">
-          {songData?.sections[1].type === "LYRICS" ? (
-            songData?.sections[1].text.map((line, i) => (
-              <p key={i} className="text-gray-400 text-base my-1">
-                {line}
-              </p>
-            ))
-          ) : (
-            <p className="text-gray-400 text-base my-1">
-              Sorry, no lyrics found!
-            </p>
-          )}
-        </div>
-      </div>
       <RelatedSongs
         data={data}
         isPlaying={isPlaying}
