9f9eb9133a67ffb26cde8c1ed5a3eea98fd5e380
gift56 <efeasiughu@gmail.com>
2022-10-15 11:06:50 +0100

    adding loader components

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -21,30 +21,30 @@ const SongDetails = () => {
     songid,
   });
 
-  if (isFetchingSongDetails || isFetchingRelatedSongs) return 
+  if (isFetchingSongDetails || isFetchingRelatedSongs) return <Loader />;
 
-    return (
-      <div className="flex flex-col">
-        <DetailsHeader artistId="" songData={songData} />
-        <div className="mb-10">
-          <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-          <div className="mt-5">
-            {songData?.sections[1].type === "LYRICS" ? (
-              songData?.sections[1].text.map((line, i) => (
-                <p key={i} className="text-gray-400 text-base my-1">
-                  {line}
-                </p>
-              ))
-            ) : (
-              <p className="text-gray-400 text-base my-1">
-                Sorry, no lyrics found!
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId="" songData={songData} />
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+        <div className="mt-5">
+          {songData?.sections[1].type === "LYRICS" ? (
+            songData?.sections[1].text.map((line, i) => (
+              <p key={i} className="text-gray-400 text-base my-1">
+                {line}
               </p>
-            )}
-          </div>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">
+              Sorry, no lyrics found!
+            </p>
+          )}
         </div>
-        <RelatedSongs />
       </div>
-    );
+      <RelatedSongs />
+    </div>
+  );
 };
 
 export default SongDetails;
