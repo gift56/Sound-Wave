543b735a1f58fd6a3d92069bf563bf9605e0ad27
gift56 <efeasiughu@gmail.com>
2022-10-15 10:05:40 +0100

    saw what was inside section

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -11,14 +11,14 @@ const SongDetails = () => {
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid });
 
+//   console.log(songData);
+
   return (
     <div className="flex flex-col">
       {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-        <div className="mt-5">
-            {songData?.sections[1]}
-        </div>
+        <div className="mt-5">{songData?.sections[1].type==="LYRICS"}</div>
       </div>
     </div>
   );
