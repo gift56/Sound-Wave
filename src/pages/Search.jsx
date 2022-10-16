0d3b872a8846941a1a6a725584926f37ed78bc01
gift56 <efeasiughu@gmail.com>
2022-10-16 13:52:14 +0100

    working on search endpoints

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,35 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
 const Search = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading top Charts" />;
+
+  if (error) return <Error />;
+
   return (
-    <div></div>
-  )
-}
+    <div className="flex flex-col">
+      <h2 className="text-3xl font-bold text-white text-left mt-4 mb-10">
+        Discover Top Charts
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            i={i}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
