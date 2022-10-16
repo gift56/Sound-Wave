5060af29dfe341400f4e9393986422bbfd0084d2
gift56 <efeasiughu@gmail.com>
2022-10-16 13:17:11 +0100

    navigated successfull

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -3,8 +3,18 @@ import { useNavigate } from "react-router-dom";
 const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <img src={track?.images?.coverart} alt="artist" />
+    <div
+      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+      onClick={() => navigate(`/artists/${track?.artist[0].adamid}`)}
+    >
+      <img
+        src={track?.images?.coverart}
+        alt="artist"
+        className="w-full h-56 rounded-lg"
+      />
+      <p className="mt-4 font-semibold text-lg truncate text-white">
+        {track?.subtitle}
+      </p>
     </div>
   );
 };
