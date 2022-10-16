68ef184789aabcdfdb2ed34b900b7b8e2527ad89
gift56 <efeasiughu@gmail.com>
2022-10-16 13:53:56 +0100

    done with endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -30,6 +30,9 @@ export const shazamCoreApi = createApi({
     getSongsByCountry: builder.query({
       query: (countryCode) => `/charts/country?country_code=${countryCode}`,
     }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) => `/search?term=${searchTerm}`,
+    }),
   }),
 });
 
