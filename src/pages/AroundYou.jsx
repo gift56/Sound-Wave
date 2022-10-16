7b4bfca9dfd265aecbec70bcfa1de4e60a5b875b
gift56 <efeasiughu@gmail.com>
2022-10-16 11:19:00 +0100

    working on daa

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -29,8 +29,10 @@ const AroundYou = () => {
       <h2 className="text-3xl font-bold text-white text-left mt-4 mb-10">
         Around You
       </h2>
-      <div>
-        
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard key={song.key} song={song} isPlaying={isPlaying} />
+        ))}
       </div>
     </div>
   );
