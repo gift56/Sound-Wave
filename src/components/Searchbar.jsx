f3c88271c8467f445573acb3039ac11d9553679e
gift56 <efeasiughu@gmail.com>
2022-10-16 13:24:43 +0100

    working on search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,17 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import { useState } from "react";
+
+const Searchbar = () => {
+  return (
+    <form
+      autoComplete="off"
+      className="p-2 text-gray-400 focus-within:text-gray-600"
+    >
+      <label htmlFor="search-field" className="sr-only">
+        Search all songs
+      </label>
+      <div className="flex justify-start items-center"></div>
+    </form>
+  );
+};
 
 export default Searchbar;
