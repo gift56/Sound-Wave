e50fb79be49d4962eb56cd666d663507ef1067bf
gift56 <efeasiughu@gmail.com>
2022-10-16 13:12:44 +0100

    working on artist area

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,7 +4,7 @@ const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <img src="" alt="" />
+      <img src={track?.images?.coverart} alt="artist" />
     </div>
   );
 };
