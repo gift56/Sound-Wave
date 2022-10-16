b27e860693f4374b87ce59a067ed878997ebd60a
gift56 <efeasiughu@gmail.com>
2022-10-16 13:46:12 +0100

    making searchicon work

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <form
       autoComplete="off"
