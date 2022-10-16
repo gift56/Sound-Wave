0bd195ba555ad19af8ca77cbaef6fee93349a9fe
gift56 <efeasiughu@gmail.com>
2022-10-16 11:26:29 +0100

    song around you in nigeria

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,34 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading top Charts" />;
+
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="text-3xl font-bold text-white text-left mt-4 mb-10">
+        Around You {` `} <span className="font-black">{country}</span>
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            i={i}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopCharts;
